845de880189671ad14874baf8d7612e2c66c0815
TSturtledove <roberttortorelli@yahoo.com>
2016-12-15 08:32:10 -0600

    got the car information to print out to html

--- quiz.js
+++ quiz.js
@@ -1,10 +1,22 @@
 var inventory = [];
 loadInventory();
 
-function populatePage (inventory) {
-  var cars = JSON.parse(inventory.target.responseText)
-  console.log("cars", cars)
+function populatePage (inry) {
+  var inventory = JSON.parse(inry.target.responseText)
+  console.log("Inventory", inventory)
   // Loop over the inventory and populate the page
+  var carsForSale = ""
+  for (var i = 0; i < inventory.cars.length; i++) {
+    carsForSale += `<div class="col-md-4">
+                      <h3>${inventory.cars[i].make}</h3>
+                      <h3>${inventory.cars[i].model}</h3>
+                      <p>${inventory.cars[i].year}</p>
+                      <p>${inventory.cars[i].price}</p>
+                      <p>${inventory.cars[i].description}</p>
+                      </div>`
+  }
+// console.log(carsForSale)
+  document.getElementById("list").innerHTML = carsForSale
 
   // Now that the DOM is loaded, establish all the event listeners needed
   // activateEvents();
@@ -15,6 +27,7 @@ function populatePage (inventory) {
 function event(){
   // loadInventory(populatePage);
 }
+
 
 //--Use callback to call the event function when the loadInventory
 // --function is call at th beginning of the page

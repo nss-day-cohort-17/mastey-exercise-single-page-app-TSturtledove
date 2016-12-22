5e9de03a584591aae65ebed74b5999617e007bee
TSturtledove <roberttortorelli@yahoo.com>
2016-12-22 00:59:36 -0600

    figured out a way to let the sleceted box change using radio buttons

--- quiz.js
+++ quiz.js
@@ -7,12 +7,14 @@ function populatePage (inry) {
   // Loop over the inventory and populate the page
   var carsForSale = ""
   for (var i = 0; i < inventory.cars.length; i++) {
-    carsForSale += `<div class="col-md-4 blackBor">
+    carsForSale += `<div class="col-md-3 blackBor">
+                      <input class="col-md-3" id="radio${[i]}" type="radio" name="card" value="all"><label for="radio${[i]}">
                       <h3>${inventory.cars[i].make}</h3>
                       <h3>${inventory.cars[i].model}</h3>
                       <p>${inventory.cars[i].year}</p>
                       <p>${inventory.cars[i].price}</p>
                       <p>${inventory.cars[i].description}</p>
+                      </label>
                       </div>`
   }
 // console.log(carsForSale)
@@ -20,6 +22,9 @@ function populatePage (inry) {
 
   // Now that the DOM is loaded, establish all the event listeners needed
   // activateEvents();
+
+  //add the eventlistener for click on the cards in here so
+  //things are loaded before you target and change stuff
 }
 
 // Load the inventory and send a callback function to be

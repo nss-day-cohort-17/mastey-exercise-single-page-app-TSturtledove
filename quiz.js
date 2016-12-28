e3b1197a8c6cddd365cf9c4323b5f3045a15cecb
TSturtledove <roberttortorelli@yahoo.com>
2016-12-28 16:42:32 -0600

    cleaning up the pages

--- quiz.js
+++ quiz.js
@@ -19,30 +19,13 @@ function populatePage (inry) {
   }
 // console.log(carsForSale)
   document.getElementById("list").innerHTML = carsForSale
-
-  // Now that the DOM is loaded, establish all the event listeners needed
-  // activateEvents();
-
-  //add the eventlistener for click on the cards in here so
-  //things are loaded before you target and change stuff
-}
-
-// Load the inventory and send a callback function to be
-// invoked after the process is complete
-function event(){
-  // loadInventory(populatePage);
 }
 
 
-//--Use callback to call the event function when the loadInventory
-// --function is call at th beginning of the page
 function loadInventory (callback) {
   var inventoryLoader = new XMLHttpRequest();
 
   inventoryLoader.addEventListener("load", populatePage
-  //function () {
-
-//  }
 );
   inventoryLoader.open("GET", "inventory.json")
   inventoryLoader.send()
@@ -56,7 +39,6 @@ function changeText() {
       var userInput = document.getElementById("userInput").value;
       // document.getElementById("test").innerHTML = userInput;
       var targetText = document.getElementById(`picked${[t]}`);
-      // return
       targetText.innerHTML = userInput;
       break;
     }

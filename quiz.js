7ffd03fe343a81621ad787644d2dc40cecf1b892
TSturtledove <roberttortorelli@yahoo.com>
2017-01-05 21:24:30 -0600

    after a long time of figuring things out with my brothers helpI got the function stuff to work

--- quiz.js
+++ quiz.js
@@ -1,45 +1,55 @@
 var inventory = [];
 loadInventory();
 
-function populatePage (inry) {
-  var inventory = JSON.parse(inry.target.responseText)
-  console.log("Inventory", inventory)
-  // Loop over the inventory and populate the page
-  var carsForSale = ""
-  for (var i = 0; i < inventory.cars.length; i++) {
-    carsForSale += `<div class="blackBor" onclick="focusing()">
-                      <input id="radio${[i]}" type="radio" name="card" value="all"><label for="radio${[i]}" class="info col-md-3">
-                      <h3>${inventory.cars[i].make}</h3>
-                      <h3>${inventory.cars[i].model}</h3>
-                      <p>${inventory.cars[i].year}</p>
-                      <p>${inventory.cars[i].price}</p>
-                      <p id="picked${[i]}">${inventory.cars[i].description}</p>
-                      </label>
-                      </div>`
-  }
-// console.log(carsForSale)
-  document.getElementById("list").innerHTML = carsForSale
-}
-
 // function populatePage (inry) {
 //   var inventory = JSON.parse(inry.target.responseText)
 //   console.log("Inventory", inventory)
 //   // Loop over the inventory and populate the page
 //   var carsForSale = ""
 //   for (var i = 0; i < inventory.cars.length; i++) {
-//     carsForSale += `<div class="blackBor">
-//                       <a href="#">
+//     carsForSale += `<div class="blackBor" onclick="focusing()">
+//                       <input id="radio${[i]}" type="radio" name="card" value="all"><label for="radio${[i]}" class="info col-md-3">
 //                       <h3>${inventory.cars[i].make}</h3>
 //                       <h3>${inventory.cars[i].model}</h3>
 //                       <p>${inventory.cars[i].year}</p>
 //                       <p>${inventory.cars[i].price}</p>
 //                       <p id="picked${[i]}">${inventory.cars[i].description}</p>
-//                       </a>
+//                       </label>
 //                       </div>`
 //   }
 // // console.log(carsForSale)
 //   document.getElementById("list").innerHTML = carsForSale
 // }
+
+function populatePage (inry) {
+  var inventory = JSON.parse(inry.target.responseText)
+  console.log("Inventory", inventory)
+  // Loop over the inventory and populate the page
+  var carsForSale = '<div class="container">'
+  var rowCount = 1
+  carsForSale += '<div class="row">' //makes a row div
+  for (var i = 0; i < inventory.cars.length; i++) {
+    carsForSale += `<div class="blackBor col-md-3">
+                      <h3>${inventory.cars[i].make}</h3>
+                      <h3>${inventory.cars[i].model}</h3>
+                      <p>${inventory.cars[i].year}</p>
+                      <p>${inventory.cars[i].price}</p>
+                      <p class="description">${inventory.cars[i].description}</p>
+                      </div>`
+
+    rowCount ++
+    if(rowCount > 3) {
+      carsForSale+= '</div>'
+      carsForSale+= '<div class="row">'
+      rowCount = 1
+    }
+  }
+  carsForSale += '</div>' //for the row
+  carsForSale += '</div>' //for the container
+// console.log(carsForSale)
+  document.getElementById("list").innerHTML = carsForSale
+  activeEvents();
+}
 
 function loadInventory (callback) {
   var inventoryLoader = new XMLHttpRequest();
@@ -51,19 +61,46 @@ function loadInventory (callback) {
 }
 
 
-function changeText() {
-  var atamoton = document.getElementsByName("card")
-  for (var t=0, length = atamoton.length; t < length; t++) {
-    if (atamoton[t].checked){
-      var userInput = document.getElementById("userInput").value;
-      // document.getElementById("test").innerHTML = userInput;
-      var targetText = document.getElementById(`picked${[t]}`);
-      targetText.innerHTML = userInput;
-      break;
-    }
+function resetDiv() {
+  var divs = document.getElementsByClassName("blackBor");
+  for (var i = 0; i < divs.length; i++) {
+    let el = divs[i]
+    el.style.background = null;
+    el.style.border = "2px solid black";
+    el.className = "blackBor col-md-3"
   }
-  // console.log("test")
+}
 
+function activeEvents() {
+  var divs = document.getElementsByClassName("blackBor");
+  for (var i = 0; i < divs.length; i++) {
+    let el = divs[i]
+    el.addEventListener("click", function() { changeElement(el, "#99b600")});
+  }
+  var textField = document.getElementById("userInput")
+  textField.addEventListener("keyup", function() { updateDescription(textField.value)});
+
+}
+
+
+// function clearTextField() {
+//   var textField = document.getElementById("userInput")
+//   textField.value = ""
+// }
+
+
+function updateDescription(value) {
+  var description = document.querySelectorAll("div.checked p.description")[0] //querySelectorAll returns an array, so I need to add the [0] on the end to select one
+  description.innerHTML = value;
+}
+
+function changeElement(el, color) {
+  resetDiv();
+  focusing();
+  // clearTextField();
+  el.style.background = color;
+  el.style.border = "5px solid black";
+  el.className += " checked" //adds new class to let us focus in on it
 }
 
 
@@ -74,12 +111,3 @@ function focusing() {
 
   console.log("focus")
 }
-//
-// function activeEvents(){
-//
-// }
-//
-// var grn = "#99b600"
-// function colorChange(,grn) {
-//
-// }
